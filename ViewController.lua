a9fa2548e72b6cba7ec1f3adeaa79a1764e58c52
marchend001 <souvenir1994@yeah.net>
2018-11-26 17:04:33 +0800

    Add files via upload

---
+++ ViewController.lua
@@ -0,0 +1,18 @@
+--require('mobdebug').start()
+
+waxClass{"ViewController"}
+
+
+
+function viewDidLoad(self)
+    self:ORIGviewDidLoad()
+    self:label():setText("This is setted by luascript")
+    
+end
+
+function executeLuascript(self)
+    local rect = CGRect(50, 500, 300, 200)
+    local lualabel = UILabel:initWithFrame(rect)
+    lualabel:setText("This is added by luascript downloaded from network")
+    self:view():addSubview(lualabel)
+end
